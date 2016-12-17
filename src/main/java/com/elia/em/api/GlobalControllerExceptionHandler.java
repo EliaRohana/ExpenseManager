2c76c218854d4fac1a5be41ab0aa5ca05a5f0769
rohaelia@gmail.com <fiss9500>
2016-12-17 15:21:23 +0200

    global exception handling

---
+++ src/main/java/com/elia/em/api/GlobalControllerExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.elia.em.api;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by Elia on 12/17/2016.
+ */
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Server ERROR")
+    public String defaultErrorHandler(HttpServletRequest request, Exception ex){
+        logger.error("server error : " + ex.getMessage());
+        return "server error";
+    }
+}
