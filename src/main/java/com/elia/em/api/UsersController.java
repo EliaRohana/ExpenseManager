a9f377a451e713fc18a7526dae369dacde273b77
rohaelia@gmail.com <fiss9500>
2017-01-27 15:38:30 +0200

    spring login ctrl

--- src/main/java/com/elia/em/api/UsersController.java
+++ src/main/java/com/elia/em/api/UsersController.java
@@ -1,6 +1,11 @@
 package com.elia.em.api;
 
+import com.elia.em.model.User;
+import com.elia.em.security.CurrentUser;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,5 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UsersController {
+
+    @RequestMapping(value = "/principal")
+    public ResponseEntity<User> currentPrincipal(@CurrentUser User currentUser) {
+        return new ResponseEntity<User>(currentUser, HttpStatus.OK);
+    }
+
 
 }
