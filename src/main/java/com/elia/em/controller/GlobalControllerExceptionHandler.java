721e3dc85f8f6c510b3c77338263c3b255ed4fd3
Elia Rohana <rohaelia@gmail.com>
2017-02-07 16:46:54 +0200

    exception handling

--- src/main/java/com/elia/em/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/com/elia/em/controller/GlobalControllerExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.validation.ValidationException;
+
 /**
  * Created by Elia on 12/17/2016.
  */
@@ -15,6 +17,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class GlobalControllerExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Server ERROR")
+    public String validationErrorHandler(HttpServletRequest request, ValidationException ex){
+        logger.error("server error : " + ex.getMessage());
+        return ex.getMessage();
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Server ERROR")
