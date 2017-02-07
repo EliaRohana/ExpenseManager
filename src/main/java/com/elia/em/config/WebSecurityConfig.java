ed9511cbfa87275b9bb4a16abc0f1144bd290e04
Elia Rohana <rohaelia@gmail.com>
2017-02-07 18:30:43 +0200

    fix security login path

--- src/main/java/com/elia/em/config/WebSecurityConfig.java
+++ src/main/java/com/elia/em/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .httpBasic()
             .and()
             .formLogin()
-            .loginPage("/login")
+            .loginPage("/em/login")
            .permitAll();
 
     }
